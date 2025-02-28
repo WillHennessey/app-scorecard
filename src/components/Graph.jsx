9b29b2c685437906a5837d03c53bcd7894c42489
whennekm <william.hennessey@wbadigitalsolutions.com>
2025-02-28 00:05:20 +0000

    Fixing Graphs.

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PureComponent } from "react";
 import {
   BarChart,
   PieChart,
@@ -8,18 +9,52 @@ import {
   Tooltip,
   Legend,
   Bar,
-  Cell,
   Pie,
+  Cell,
   ResponsiveContainer,
 } from "recharts";
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#AF19FF"];
 
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
 const budgetData = [
   { name: "Storage", value: 500 },
   { name: "Compute", value: 600 },
-  { name: "Networking", value: 300 },
-  { name: "Databases", value: 200 },
+  { name: "Network", value: 300 },
+  { name: "Database", value: 200 },
+];
+
+const securityAlertsBySubscriptionData = [
+  { name: "Sub 1", value: 30 },
+  { name: "Sub 2", value: 20 },
+  { name: "Sub 3", value: 10 },
+  { name: "Sub 4", value: 20 },
+  { name: "Sub 5", value: 100 },
 ];
 
 const costData = [
@@ -34,32 +69,24 @@ const costData = [
 const resourceData = [
   { service: "Storage", usage: 30 },
   { service: "Compute", usage: 45 },
-  { service: "Networking", usage: 15 },
-  { service: "Databases", usage: 10 },
+  { service: "Network", usage: 15 },
+  { service: "Database", usage: 10 },
 ];
 
 const securityAlertsByResourceData = [
   { resourceType: "VM", alerts: 10 },
-  { resourceType: "SQL Database", alerts: 5 },
-  { resourceType: "Web Apps", alerts: 8 },
-  { resourceType: "Storage Accounts", alerts: 12 },
-  { resourceType: "Cosmos DB", alerts: 2 },
+  { resourceType: "SQL", alerts: 5 },
+  { resourceType: "Web", alerts: 8 },
+  { resourceType: "Storage", alerts: 12 },
+  { resourceType: "Cosmos", alerts: 2 },
 ];
 
 const top5SecurityAlertsData = [
-  { alertName: "Alert A", count: 20 },
-  { alertName: "Alert B", count: 15 },
-  { alertName: "Alert C", count: 10 },
-  { alertName: "Alert D", count: 5 },
-  { alertName: "Alert E", count: 25 },
-];
-
-const securityAlertsBySubscriptionData = [
-  { subscription: "Sub 1", count: 30 },
-  { subscription: "Sub 2", count: 20 },
-  { subscription: "Sub 3", count: 10 },
-  { subscription: "Sub 4", count: 20 },
-  { subscription: "Sub 5", count: 100 },
+  { name: "Alert A", count: 20 },
+  { name: "Alert B", count: 15 },
+  { name: "Alert C", count: 10 },
+  { name: "Alert D", count: 5 },
+  { name: "Alert E", count: 25 },
 ];
 
 const top5PolicyViolationsData = [
@@ -71,159 +98,155 @@ const top5PolicyViolationsData = [
 ];
 
 const top5RegulatoryComplianceAlertsData = [
-  { alert: "Alert 1", count: 4 },
-  { alert: "Alert 2", count: 3 },
-  { alert: "Alert 3", count: 5 },
-  { alert: "Alert 4", count: 2 },
-  { alert: "Alert 5", count: 7 },
+  { name: "Alert 1", count: 4 },
+  { name: "Alert 2", count: 3 },
+  { name: "Alert 3", count: 5 },
+  { name: "Alert 4", count: 2 },
+  { name: "Alert 5", count: 7 },
 ];
 
 const regulatoryComplianceBySubscriptionData = [
-  { subscription: "Sub 1", compliance: 20 },
-  { subscription: "Sub 2", compliance: 40 },
-  { subscription: "Sub 3", compliance: 30 },
-  { subscription: "Sub 4", compliance: 10 },
+  { name: "Sub 1", value: 20 },
+  { name: "Sub 2", value: 40 },
+  { name: "Sub 3", value: 30 },
+  { name: "Sub 4", value: 10 },
 ];
 
-const RADIAN = Math.PI / 180;
+class Graph extends PureComponent {
+  renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos((-midAngle * Math.PI) / 180);
+    const y = cy + radius * Math.sin((-midAngle * Math.PI) / 180);
 
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  index,
-}) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
 
-  return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
+  getGraphData = (title) => {
+    switch (title) {
+      case "Monthly Spend":
+        return { data: costData, type: "monthlySpend" };
+      case "Top 5 Services":
+        return { data: resourceData, type: "topServices" };
+      case "Cost by Subscription":
+        return { data: budgetData, type: "pie" };
+      case "Security Alerts by Resource Type":
+        return { data: securityAlertsByResourceData, type: "bar" };
+      case "Top 5 Security Alerts":
+        return { data: top5SecurityAlertsData, type: "horizontalBar" };
+      case "Security Alerts by Subscription":
+        return { data: securityAlertsBySubscriptionData, type: "pie" };
+      case "Top 5 Azure Policy Violations":
+        return { data: top5PolicyViolationsData, type: "horizontalBar" };
+      case "Top 5 Regulatory Compliance Alerts":
+        return {
+          data: top5RegulatoryComplianceAlertsData,
+          type: "horizontalBar",
+        };
+      case "Regulatory Compliance by Subscription":
+        return { data: regulatoryComplianceBySubscriptionData, type: "pie" };
+      default:
+        return null;
+    }
+  };
 
-function Graph({ title }) {
-  let graphData;
-  let graphType;
+  render() {
+    const { title } = this.props;
+    const graphInfo = this.getGraphData(title);
 
-  switch (title) {
-    case "Monthly Spend":
-      graphData = costData;
-      graphType = "monthlySpend";
-      break;
-    case "Top 5 Services":
-      graphData = resourceData;
-      graphType = "topServices";
-      break;
-    case "Cost by Subscription":
-      graphData = budgetData;
-      graphType = "costBySubscription";
-      break;
-    case "Security Alerts by Resource Type":
-      graphData = securityAlertsByResourceData;
-      graphType = "bar";
-      break;
-    case "Top 5 Security Alerts":
-      graphData = top5SecurityAlertsData;
-      graphType = "horizontalBar";
-      break;
-    case "Security Alerts by Subscription":
-      graphData = securityAlertsBySubscriptionData;
-      graphType = "pie";
-      break;
-    case "Top 5 Azure Policy Violations":
-      graphData = top5PolicyViolationsData;
-      graphType = "horizontalBar";
-      break;
-    case "Top 5 Regulatory Compliance Alerts":
-      graphData = top5RegulatoryComplianceAlertsData;
-      graphType = "horizontalBar";
-      break;
-    case "Regulatory Compliance by Subscription":
-      graphData = regulatoryComplianceBySubscriptionData;
-      graphType = "pie";
-      break;
-    default:
-      return null;
+    if (!graphInfo) return null;
+
+    const { data, type } = graphInfo;
+
+    return (
+      <div>
+        <h3 className="text-center font-bold">{title}</h3>
+        <ResponsiveContainer width="100%" height={300}>
+          {type === "monthlySpend" && (
+            <BarChart data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="amount" fill="#0088FE" />
+            </BarChart>
+          )}
+          {type === "topServices" && (
+            <BarChart data={data} layout="vertical">
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="number" domain={[0, 50]} />
+              <YAxis dataKey="service" type="category" />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="usage" fill="#00C49F" />
+            </BarChart>
+          )}
+          {type === "bar" && (
+            <BarChart data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="resourceType" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="alerts" fill="#0088FE" />
+            </BarChart>
+          )}
+          {type === "horizontalBar" && (
+            <BarChart data={data} layout="vertical">
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="number" />
+              <YAxis
+                dataKey={title.includes("Violation") ? "violation" : "name"}
+                type="category"
+              />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="count" fill="#82ca9d" />
+            </BarChart>
+          )}
+          {type === "pie" && (
+            <PieChart>
+              <Pie
+                dataKey="value"
+                nameKey="name"
+                data={data}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+              />
+              {data.map((index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          )}
+        </ResponsiveContainer>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <h3 className="text-center font-bold">{title}</h3>
-      {graphType === "monthlySpend" && (
-        <BarChart width={375} height={250} data={graphData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="amount" fill="#0088FE" />
-        </BarChart>
-      )}
-      {graphType === "topServices" && (
-        <BarChart width={375} height={250} data={graphData} layout="vertical">
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis type="number" domain={[0, 50]} />
-          <YAxis dataKey="service" type="category" />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="usage" fill="#00C49F" />
-        </BarChart>
-      )}
-      {graphType === "costBySubscription" && (
-        <ResponsiveContainer width="100%" height="100%">
-          <PieChart width={375} height={250}>
-            <Tooltip />
-            <Legend />
-            <Pie dataKey="value" data={graphData} fill="#8884d8" label />
-          </PieChart>
-        </ResponsiveContainer>
-      )}
-      {graphType === "bar" && (
-        <BarChart width={375} height={250} data={graphData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="resourceType" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="alerts" fill="#0088FE" />
-        </BarChart>
-      )}
-      {graphType === "horizontalBar" && (
-        <BarChart width={375} height={250} data={graphData} layout="vertical">
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis type="number" />
-          <YAxis
-            dataKey={title.includes("Violation") ? "violation" : "alertName"}
-            type="category"
-          />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="count" fill="#82ca9d" />
-        </BarChart>
-      )}
-      {graphType === "pie" && (
-        <ResponsiveContainer width="100%" height="100%">
-          <PieChart width={375} height={250}>
-            <Tooltip />
-            <Legend />
-            <Pie dataKey="value" data={graphData} fill="#8884d8" label />
-          </PieChart>
-        </ResponsiveContainer>
-      )}
-    </div>
-  );
 }
 
 export default Graph;
